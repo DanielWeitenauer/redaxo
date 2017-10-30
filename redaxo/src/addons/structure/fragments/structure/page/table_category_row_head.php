2b19ce81eec137395e5bce1f36f7ead64f295414
Daniel Weitenauer <github@studio-ahoi.de>
2017-10-30 16:38:02 +0100

    BC break: rename css classes

--- redaxo/src/addons/structure/fragments/structure/page/table_category_row_head.php
+++ redaxo/src/addons/structure/fragments/structure/page/table_category_row_head.php
@@ -2,6 +2,6 @@
     <th class="rex-table-icon"><?=$this->table_icon;?></th>
     <th class="rex-table-id"><?=rex_i18n::msg('header_id');?></th>
     <th><?=rex_i18n::msg('header_category');?></th>
-    <th class="rex-table-priority"><?=rex_i18n::msg('header_priority');?></th>
+    <th class="rex-table-infos"><?=rex_i18n::msg('header_infos');?></th>
     <th class="rex-table-action"><?=rex_i18n::msg('header_status');?></th>
 </tr>
