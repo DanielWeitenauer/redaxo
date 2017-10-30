69666d6c3aae5db2422f2d5e885c902e4f826d63
Daniel Weitenauer <github@studio-ahoi.de>
2017-10-30 22:54:45 +0100

    Prevent empty page after deleting article in content/edit

--- redaxo/src/addons/structure/lib/structure_actions/structure_article_delete.php
+++ redaxo/src/addons/structure/lib/structure_actions/structure_article_delete.php
@@ -13,9 +13,13 @@ class rex_structure_article_delete extends rex_fragment
             return '';
         }
 
+        $category_id = rex_article::get($this->edit_id)->getCategoryId();
+
         $url_params = array_merge($this->url_params, [
+            'page' => 'structure',
             'rex-api-call' => 'article_delete',
             'article_id' => $this->edit_id,
+            'category_id' => $category_id,
         ]);
 
         return '<a class="btn btn-default" href="'.$this->context->getUrl($url_params).'" data-confirm="'.rex_i18n::msg('delete').'?" title="'.rex_i18n::msg('delete').'"><i class="rex-icon rex-icon-delete"></i></a>';
