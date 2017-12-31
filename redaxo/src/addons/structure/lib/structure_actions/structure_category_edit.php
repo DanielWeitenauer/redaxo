828853b4779653ab435314b4d04aaab5e8c0f2b6
Daniel Weitenauer <github@studio-ahoi.de>
2017-12-31 02:09:46 +0100

    Fix var names

--- redaxo/src/addons/structure/lib/structure_actions/structure_category_edit.php
+++ redaxo/src/addons/structure/lib/structure_actions/structure_category_edit.php
@@ -11,6 +11,8 @@ class rex_structure_category_edit extends rex_structure_action_field
     public function get()
     {
         $category_id = $this->getVar('edit_id');
+        /** @var rex_context $context */
+        $context = $this->getVar('context');
 
         if (!rex::getUser()->getComplexPerm('structure')->hasCategoryPerm($category_id)) {
             return '';
@@ -30,7 +32,7 @@ class rex_structure_category_edit extends rex_structure_action_field
             $url_params = array_merge($this->getVar('url_params'), [
                 'form_category_edit' => $category_id,
             ]);
-            $button_params['url'] = $this->getVar('context')->getUrl($url_params, false);
+            $button_params['url'] = $context->getUrl($url_params, false);
             $button_params['attributes']['class'][] = 'btn-default';
         }
 
@@ -46,19 +48,25 @@ class rex_structure_category_edit extends rex_structure_action_field
 
     /**
      * @return string
+     * @throws rex_sql_exception
      */
     protected function getModal()
     {
         $category_id = $this->getVar('edit_id');
-        $sql = $this->getVar('sql');
 
         if (!rex::getUser()->getComplexPerm('structure')->hasCategoryPerm($category_id)) {
             return '';
         }
 
+        /** @var rex_sql $sql */
+        $sql = $this->getVar('sql');
+        /** @var rex_context $context */
+        $context = $this->getVar('context');
         $clang = rex_request('clang', 'int');
         $clang = rex_clang::exists($clang) ? $clang : rex_clang::getStartId();
         $data_colspan = 5; // Only for BC reasons
+        $cat_name = $sql->getValue('catname');
+        $cat_priority = $sql->getValue('catpriority');
 
         // Extension point
         $cat_form_buttons = rex_extension::registerPoint(new rex_extension_point('CAT_FORM_BUTTONS', '', [
@@ -71,15 +79,15 @@ class rex_structure_category_edit extends rex_structure_action_field
             'id' => $category_id,
             'clang' => $clang,
             'category' => $sql,
-            'catname' => $sql->getValue('catname'),
-            'catpriority' => $sql->getValue('catpriority'),
+            'catname' => $cat_name,
+            'catpriority' => $cat_priority,
             'data_colspan' => $data_colspan + 1,
         ]));
 
         return '  
             <div class="modal fade" id="category-edit-'.$category_id.'">
                 <div class="modal-dialog">
-                    <form id="rex-form-category-move-'.$category_id.'" class="modal-content form-vertical" action="'.$this->getUrl().'" method="post" enctype="multipart/form-data" data-pjax-container="#rex-page-main">
+                    <form id="rex-form-category-move-'.$category_id.'" class="modal-content form-vertical" action="'.$context->getUrl().'" method="post" enctype="multipart/form-data" data-pjax-container="#rex-page-main">
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span></button>
                             <h3 class="modal-title">'.rex_i18n::msg('header_category').'</h3>
@@ -95,11 +103,11 @@ class rex_structure_category_edit extends rex_structure_action_field
                                     </dl>
                                     <dl class="rex-form-group form-group">
                                         <dt><label for="category-name">'.rex_i18n::msg('header_category').'</label></dt>
-                                        <dd><input class="form-control rex-js-autofocus" type="text" name="category-name" value="'.htmlspecialchars($this->getSqlValue('catname')).'" autofocus /></dd>
+                                        <dd><input class="form-control rex-js-autofocus" type="text" name="category-name" value="'.htmlspecialchars($cat_name).'" autofocus /></dd>
                                     </dl>
                                     <dl class="rex-form-group form-group">
                                         <dt><label for="category-position">'.rex_i18n::msg('header_priority').'</label></dt>
-                                        <dd><input class="form-control" type="text" name="category-position" value="'.htmlspecialchars($this->getSqlValue('catpriority')).'" /></dd>
+                                        <dd><input class="form-control" type="text" name="category-position" value="'.htmlspecialchars($cat_priority).'" /></dd>
                                     </dl>
                                     '.$cat_form_buttons.'
                                     '.$cat_form_edit.'
