009da06b17e2be08efe1c57cd3b80fceff90fd44
Daniel Weitenauer <github@studio-ahoi.de>
2018-01-01 19:45:48 +0100

    Improve styling

--- redaxo/src/addons/structure/plugins/structure_prio/boot.php
+++ redaxo/src/addons/structure/plugins/structure_prio/boot.php
@@ -3,17 +3,22 @@
  * @author Daniel Weitenauer
  * @copyright (c) 2017 studio ahoi
  */
+if (rex::isBackend() && rex::getUser()) {
+    if (rex_be_controller::getCurrentPagePart(1) == 'structure') {
+        rex_view::addCssFile($this->getAssetsUrl('fix_be_styles.css'));
+    }
 
-rex_extension::register('PAGE_STRUCTURE_ARTICLE_ACTIONS', function (rex_extension_point $ep) {
-    /** @var rex_structure_action_row $article_rows */
-    $article_rows = $ep->getSubject();
-    $action_params = $ep->getParam('action_vars');
+    rex_extension::register('PAGE_STRUCTURE_ARTICLE_ACTIONS', function(rex_extension_point $ep) {
+        /** @var rex_structure_action_row $article_rows */
+        $article_rows  = $ep->getSubject();
+        $action_params = $ep->getParam('action_vars');
 
-    $article_rows->setColumn('priority', new rex_structure_action_column());
-    $article_rows->getColumn('priority')
-        ->setField('article_priority_dec', new rex_structure_article_priority_dec($action_params))
-        ->setField('article_priority', new rex_structure_article_priority_ext($action_params))
-        ->setField('article_priority_inc', new rex_structure_article_priority_inc($action_params));
+        $article_rows->setColumn('priority', rex_structure_action_column::factory($action_params));
+        $article_rows->getColumn('priority')
+                     ->setField('article_priority_dec', rex_structure_article_priority_dec::factory($action_params))
+                     ->setField('article_priority', rex_structure_article_priority_ext::factory($action_params))
+                     ->setField('article_priority_inc', rex_structure_article_priority_inc::factory($action_params));
 
-    return $article_rows;
-});
+        return $article_rows;
+    });
+}
