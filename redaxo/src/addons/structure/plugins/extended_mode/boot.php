412b8df679d05a839bf0e59c0b910af77d3c03a4
Daniel Weitenauer <github@studio-ahoi.de>
2018-01-01 19:45:36 +0100

    Improve styling

--- redaxo/src/addons/structure/plugins/extended_mode/boot.php
+++ redaxo/src/addons/structure/plugins/extended_mode/boot.php
@@ -4,50 +4,63 @@
  * @copyright (c) 2017 studio ahoi
  */
 
-rex_extension::register('PAGE_STRUCTURE_CATEGORY_ACTIONS', function (rex_extension_point $ep) {
-    /** @var rex_structure_action_row $category_row */
-    $category_row = $ep->getSubject();
-    $action_vars = $ep->getParam('action_vars');
-    $category = rex_category::get($action_vars['edit_id']);
-
-    if ($category instanceof rex_category) {
-        $category_row->getColumn('action')
-            ->setField('category2article', new rex_structure_category2article($action_vars))
-            ->setField('category_move', new rex_structure_category_move($action_vars));
+if (rex::isBackend() && rex::getUser()) {
+    if (rex_be_controller::getCurrentPagePart(1) == 'structure') {
+        rex_view::addCssFile($this->getAssetsUrl('fix_be_styles.css'));
     }
 
-    /** @var rex_structure_action_column $column */
-    foreach ($category_row->getColumns() as $column) {
-        /** @var rex_structure_action_field $field */
-        foreach ($column->getFields() as $field) {
-            $field->setVar('hide_label', true);
+    rex_extension::register('PAGE_STRUCTURE_CATEGORY_ACTIONS', function(rex_extension_point $ep) {
+        /** @var rex_structure_action_row $category_row */
+        $category_row = $ep->getSubject();
+        $action_vars  = $ep->getParam('action_vars');
+        $category     = rex_category::get($action_vars['edit_id']);
+
+        if (!$category_row->hasColumn('action')) {
+            $category_row->setColumn('action', rex_structure_action_column::factory($action_vars));
         }
-    }
-
-    return $category_row;
-});
-
-rex_extension::register('PAGE_STRUCTURE_ARTICLE_ACTIONS', function (rex_extension_point $ep) {
-    /** @var rex_structure_action_row $article_row */
-    $article_row = $ep->getSubject();
-    $action_vars = $ep->getParam('action_vars');
-    $article = rex_article::get($action_vars['edit_id']);
-
-    if ($article instanceof rex_article) {
-        $article_row->getColumn('action')
-            ->setField('article2category', new rex_structure_article2category($action_vars))
-            ->setField('article2startarticle',  new rex_structure_article2startarticle($action_vars))
-            ->setField('article_move',  new rex_structure_article_move($action_vars))
-            ->setField('article_copy',  new rex_structure_article_copy($action_vars));
-    }
-
-    /** @var rex_structure_action_column $column */
-    foreach ($article_row->getColumns() as $column) {
-        /** @var rex_structure_action_field $field */
-        foreach ($column->getFields() as $field) {
-            $field->setVar('hide_label', true);
+        if ($category instanceof rex_category) {
+            $category_row->getColumn('action')
+                         ->setField('category2article', rex_structure_category2article::factory($action_vars))
+                         ->setField('category_move', rex_structure_category_move::factory($action_vars));
         }
-    }
 
-    return $article_row;
-});
+        /** @var rex_structure_action_column $column */
+        foreach ($category_row->getColumns() as $column) {
+            /** @var rex_structure_action_field $field */
+            foreach ($column->getFields() as $field) {
+                $field->setVar('hide_label', true);
+            }
+        }
+
+        return $category_row;
+    });
+
+    rex_extension::register('PAGE_STRUCTURE_ARTICLE_ACTIONS', function(rex_extension_point $ep) {
+        /** @var rex_structure_action_row $article_row */
+        $article_row = $ep->getSubject();
+        $action_vars = $ep->getParam('action_vars');
+        $article = rex_article::get($action_vars['edit_id']);
+
+
+        if (!$article_row->hasColumn('action')) {
+            $article_row->setColumn('action', rex_structure_action_column::factory($action_vars));
+        }
+        if ($article instanceof rex_article) {
+            $article_row->getColumn('action')
+                ->setField('article2category', rex_structure_article2category::factory($action_vars))
+                ->setField('article2startarticle', rex_structure_article2startarticle::factory($action_vars))
+                ->setField('article_move', rex_structure_article_move::factory($action_vars))
+                ->setField('article_copy', rex_structure_article_copy::factory($action_vars));
+        }
+
+        /** @var rex_structure_action_column $column */
+        foreach ($article_row->getColumns() as $column) {
+            /** @var rex_structure_action_field $field */
+            foreach ($column->getFields() as $field) {
+                $field->setVar('hide_label', true);
+            }
+        }
+
+        return $article_row;
+    });
+}
