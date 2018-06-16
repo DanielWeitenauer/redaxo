44f80e2c02d85a6a3b47fd3b480980472c00a923
Daniel Weitenauer <github@studio-ahoi.de>
2018-06-16 22:49:45 +0200

    Remove redundant code

--- redaxo/src/addons/structure/pages/index.php
+++ redaxo/src/addons/structure/pages/index.php
@@ -485,53 +485,29 @@ if ($category_id > 0 || ($category_id == 0 && !rex::getUser()->getComplexPerm('s
                 'catstart' => $catstart,
             ],
         ];
-        if ($KATPERM) {
-            // --------------------- ARTIKEL NORMAL VIEW | EDIT AND ENTER
-            $article_status = rex_structure_field_article_status::factory($article_vars)->getField();
-            $article_delete = rex_structure_field_article_delete::factory($article_vars)->getField();
-            $article_edit = rex_structure_field_article_edit::factory($article_vars)->getField();
-            $article_name = rex_structure_field_article_name::factory($article_vars)->getField();
-            $article_icon = rex_structure_field_article_icon::factory($article_vars)->getField();
-            $article_id_field = rex_structure_field_article_id::factory($article_vars)->getField();
-            $article_template = rex_structure_field_article_template::factory($article_vars)->getField();
-            $article_create_date = rex_structure_field_article_create_date::factory($article_vars)->getField();
-            $article_priority = rex_structure_field_article_priority::factory($article_vars)->getField();
 
-            $echo .= '<tr' . (($class_startarticle != '') ? ' class="' . trim($class_startarticle) . '"' : '') . '>
-                            <td class="rex-table-icon">'.$article_icon.'</td>
-                            <td class="rex-table-id" data-title="' . rex_i18n::msg('header_id') . '">'.$article_id_field.'</td>
-                            <td data-title="' . rex_i18n::msg('header_article_name') . '">'.$article_name.'</td>
-                            <td data-title="' . rex_i18n::msg('header_template') . '">'.$article_template.'</td>
-                            <td data-title="' . rex_i18n::msg('header_date') . '">'.$article_create_date.'</td>
-                            <td class="rex-table-priority" data-title="' . rex_i18n::msg('header_priority') . '">'.$article_priority.'</td>
-                            <td class="rex-table-action">'.$article_edit.'</td>
-                            <td class="rex-table-action">'.$article_delete.'</td>
-                            <td class="rex-table-action">'.$article_status.'</td>
-                        </tr>
-                        ';
-        } else {
-            // --------------------- ARTIKEL NORMAL VIEW | NO EDIT NO ENTER
-            $article_status = rex_structure_field_article_status::factory($article_vars)->getField();
-            $article_delete = rex_structure_field_article_delete::factory($article_vars)->getField();
-            $article_edit = rex_structure_field_article_edit::factory($article_vars)->getField();
-            $article_name = rex_structure_field_article_name::factory($article_vars)->getField();
-            $article_icon = rex_structure_field_article_icon::factory($article_vars)->getField();
-            $article_id_field = rex_structure_field_article_id::factory($article_vars)->getField();
-            $article_template = rex_structure_field_article_template::factory($article_vars)->getField();
-            $article_create_date = rex_structure_field_article_priority::factory($article_vars)->getField();
+        $article_status = rex_structure_field_article_status::factory($article_vars)->getField();
+        $article_delete = rex_structure_field_article_delete::factory($article_vars)->getField();
+        $article_edit = rex_structure_field_article_edit::factory($article_vars)->getField();
+        $article_name = rex_structure_field_article_name::factory($article_vars)->getField();
+        $article_icon = rex_structure_field_article_icon::factory($article_vars)->getField();
+        $article_id_field = rex_structure_field_article_id::factory($article_vars)->getField();
+        $article_template = rex_structure_field_article_template::factory($article_vars)->getField();
+        $article_create_date = rex_structure_field_article_create_date::factory($article_vars)->getField();
+        $article_priority = rex_structure_field_article_priority::factory($article_vars)->getField();
 
-            $echo .= '<tr>
-                            <td class="rex-table-icon">'.$article_icon.'</td>
-                            <td class="rex-table-id" data-title="' . rex_i18n::msg('header_id') . '">'.$article_id_field.'</td>
-                            <td data-title="' . rex_i18n::msg('header_article_name') . '">'.$article_name.'</td>
-                            <td data-title="' . rex_i18n::msg('header_template') . '">'.$article_template.'</td>
-                            <td data-title="' . rex_i18n::msg('header_date') . '">'.$article_create_date.'</td>
-                            <td class="rex-table-priority" data-title="' . rex_i18n::msg('header_priority') . '">'.$article_priority.'</td>
-                            <td class="rex-table-action">'.$article_edit.'</td>
-                            <td class="rex-table-action">'.$article_delete.'</td>
-                            <td class="rex-table-action">'.$article_status.'</td>
-                        </tr>';
-        }
+        $echo .= '<tr' . (($class_startarticle != '') ? ' class="' . trim($class_startarticle) . '"' : '') . '>
+                        <td class="rex-table-icon">'.$article_icon.'</td>
+                        <td class="rex-table-id" data-title="' . rex_i18n::msg('header_id') . '">'.$article_id_field.'</td>
+                        <td data-title="' . rex_i18n::msg('header_article_name') . '">'.$article_name.'</td>
+                        <td data-title="' . rex_i18n::msg('header_template') . '">'.$article_template.'</td>
+                        <td data-title="' . rex_i18n::msg('header_date') . '">'.$article_create_date.'</td>
+                        <td class="rex-table-priority" data-title="' . rex_i18n::msg('header_priority') . '">'.$article_priority.'</td>
+                        <td class="rex-table-action">'.$article_edit.'</td>
+                        <td class="rex-table-action">'.$article_delete.'</td>
+                        <td class="rex-table-action">'.$article_status.'</td>
+                    </tr>
+                    ';
 
         $sql->next();
     }
